b4d4aebe94e13d030267a3aa868248a2c1f9c5b8
vdella <vitor.origamer@gmail.com>
2023-09-24 21:21:34 -0300

    applied external stylesheets to app
    	* H1 is also now cyan and bigger.
    	* The Input field is justified at the center,
    	  has a placeholder and reads the user's input
    	  with Return key events.

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 from dash import Dash, html, dcc
 from src.components.dash_tree.figure import figure
+import dash_bootstrap_components as dbc
 
+external_stylesheets = [dbc.themes.CERULEAN]
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 
-app = Dash(__name__)
+app.layout = html.Div(
+    html.Div([
+        html.H1(children='Rosetta', style={'textAlign': 'center'}),
 
-app.layout = html.Div([
-    html.H1(children='Rosetta', style={'textAlign': 'center'}),
-    dcc.Input(style={'textAlign': 'center'}),
-    dcc.Graph(figure=figure())
-])
+        html.Br(),
+        html.Br(),
+
+        html.Div(children=[
+            dcc.Input(
+                placeholder='Type your favorite regex!',
+                debounce=True),
+        ], style={"display": "flex", "justifyContent": "center"},
+        ),
+
+        html.Br(),
+
+        html.Div(children=[dcc.Graph(figure=figure())])
+    ]))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
