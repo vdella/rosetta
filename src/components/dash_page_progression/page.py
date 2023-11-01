82b63ab3cc8c76a46624c46a102374783daaffbd
vdella <vitor.origamer@gmail.com>
2023-11-01 18:53:43 -0300

    corrected page_quantity()
    	* There is no need for page_quantity() to
    	  return its own number + 1, as the
    	  + 1 page will be the exact same as its
    	  antecessor.

--- src/components/dash_page_progression/page.py
+++ src/components/dash_page_progression/page.py
@@ -8,20 +8,13 @@ class DashPage:
         self.finalized_tree = DashTree(regex)
         self.final_figure = figure_from(self.finalized_tree.regex)
 
-        self.tree_pages = [self.final_figure for _ in range(self.page_quantity())]
-
     @staticmethod
     def empty_dash_page():
         return DashPage('a#')
 
     def page_quantity(self):
-        """Returns the quantity of tree vertices to be iterated plus the final and complete figure."""
-        return len(self.finalized_tree.vertices()) + 1
-
-    def color_figures_by_actual_node(self):
-
-        for index, tree_figure in enumerate(self.tree_pages):
-            pass
+        """Returns the quantity of tree vertices to be iterated."""
+        return len(self.finalized_tree.vertices())
 
 
 if __name__ == '__main__':
