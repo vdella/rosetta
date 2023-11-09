dd299e21bc9b03b4631f6594235cf92995c96f7d
vdella <vitor.origamer@gmail.com>
2023-11-08 21:06:56 -0300

    added FA as state diagram and table

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from dash import Dash, html, dcc, Input, Output, callback, dash
+
+from src.components.dash_fa.figure import fa_state_diagram_from
+from src.components.dash_fa.table import fa_table_data_from
 from src.components.dash_page_progression.page import DashPage
 from src.components.dash_follow_pos_table.table import follow_pos_data_from
 import dash_bootstrap_components as dbc
@@ -78,42 +81,76 @@ app.layout = html.Div(
             ],
             style={'display': 'flex', 'justifyContent': 'center'},
             hidden=True,
-        )
-    ]))
+        ),
 
+        html.Br(),
+        html.Br(),
+
+        html.Div(
+            id='fa-parent',
+            children=[
+                dash.dash_table.DataTable(
+                    id='fa-table',
+                    columns=[{'name': i, 'id': i} for i in ['(source_state, symbol)', 'destiny_state']],
+                    data=[],
+                ),
+
+                html.Img(
+                    id='fa-img',
+                    src='',
+                    style={"margin-left": "100px"}
+                )
+            ],
+            style={'display': 'flex', 'justifyContent': 'center'},
+            hidden=True,
+        ),
+    ])
+)
 
 @callback(
     Output('figure-parent', 'hidden'),
     Output('tree-page-handler', 'hidden'),
     Output('follow-pos-table-parent', 'hidden'),
+    Output('fa-parent', 'hidden'),
 
     Output('pagination', 'max_value'),
     Output('bin-tree', 'figure', allow_duplicate=True),
     Output('follow-pos-table', 'data'),
+    Output('fa-table', 'data'),
+    Output('fa-img', 'src'),
 
     Input('regex-input', 'value'),
     prevent_initial_call=True)
 def create_figure_from(user_text_entry):
-    hidden_figure, hidden_page_handler, hidden_follow_pos_figure = True, True, True
+    hidden_figure, hidden_page_handler, hidden_follow_pos, hidden_fa_table = True, True, True, True
 
     if user_text_entry:
         global page
         page = DashPage(user_text_entry)
 
+        fa_diagram = fa_state_diagram_from(page.finalized_tree)
+        fa_diagram.render()
+
         return (not hidden_figure,
                 not hidden_page_handler,
-                not hidden_follow_pos_figure,
+                not hidden_follow_pos,
+                not hidden_fa_table,
 
                 page.page_quantity(),
                 page.final_figure,
-                follow_pos_data_from(page.finalized_tree))
+                follow_pos_data_from(page.finalized_tree),
+                fa_table_data_from(page.finalized_tree),
+                app.get_asset_url('finite-automata.gv.png'))
     return (hidden_figure,
             hidden_page_handler,
-            hidden_follow_pos_figure,
+            hidden_follow_pos,
+            hidden_fa_table,
 
             0,
             {},
-            [])
+            [],
+            [],
+            '')
 
 
 @callback(
